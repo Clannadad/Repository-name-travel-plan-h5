1fd9db3ebdb8ebab228b7303f53d8c10306c6d1f
Clannadad <136559553+Clannadad@users.noreply.github.com>
2026-05-06 15:44:47 +0800

    Delete travel-plan-netlify-supabase directory

--- travel-plan-netlify-supabase/js/config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://mvpzodswcuiepawnwslt.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_GX-iyuiAZDnLlHY3sb7C8g_2wSOpu9v";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
